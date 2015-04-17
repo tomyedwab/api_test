858e43a7c95d0aaf39b33ea97ce9cf25c09b1d56
Tom Yedwab <tom@khanacademy.org>
2015-04-17 15:58:50 -0700

    Saved foo/bar.sql from BigQuery.

--- foo/bar.sql
+++ foo/bar.sql
@@ -1,7 +1,3 @@
 // Title: FooBar
 // Description: Test description
-// test
-SELECT email, CONCAT('norm:', LOWER(email)) AS norm_email
-   FROM [tomyedwab.donors_choose_emails_4_6]
-   
-   GROUP BY email, norm_email;
+SELECT * FROM [latest_content.Video];
