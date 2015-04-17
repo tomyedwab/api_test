f5304c820604da9e9a125918f265bc766a8bfd55
Tom Yedwab <tom@khanacademy.org>
2015-04-17 16:31:25 -0700

    Saved foo/bar.sql from BigQuery.

--- foo/bar.sql
+++ foo/bar.sql
@@ -1,14 +1,4 @@
 // Title: FooBar
-// Description: Test description (ha!)
+// Description: Test description
 
-SELECT COUNT(*)
-FROM (
-  SELECT IFNULL(STRING(user_key.id), user_key.name) AS user_id
-  FROM [2015_04_04.BaseUserMission]
-  WHERE class = 'PersonalMission') pm
-INNER JOIN (
-  SELECT REGEXP_EXTRACT(__key__.path, '"UserData", "([^"]+)"') AS user_id
-  FROM [2015_04_04_transformed.UserDataInfo]
-  WHERE personal_recommendations_migrated != true
-  GROUP EACH BY user_id) udi
-ON pm.user_id = udi.user_id;
+SELECT * FROM [latest_content.Video];
